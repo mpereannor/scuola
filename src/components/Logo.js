9ce1f7d21d5b241a6f8e67e16d5d8aba9a349da1
chudnofskyATD <mpereannor@gmail.com>
2020-11-22 23:04:52 +0000

    feature: scuola logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Logo = (props) => {
+  return (
+    <img
+      alt="Logo"
+      src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjAgMTIuODc1djUuMDY4YzAgMi43NTQtNS43ODkgNC4wNTctOSA0LjA1Ny0zLjA1MiAwLTktMS4zOTItOS00LjA1N3YtNi4yOTRsOSA0Ljg2MyA5LTMuNjM3em0tOC4wODMtMTAuODc1bC0xMi45MTcgNS43NSAxMiA2LjUgMTEtNC40MTd2Ny4xNjdoMnYtOC4yNWwtMTIuMDgzLTYuNzV6bTEzLjA4MyAyMGgtNGMuNTc4LTEgMS0yLjUgMS00aDJjMCAxLjUxNi4zOTEgMi44NTkgMSA0eiIvPjwvc3ZnPg=="
+      {...props}
+    />
+  );
+};
+
+export default Logo;
