f2ce2b1e868e1303eabfefc9879c62d6cb490272
chudnofskyATD <mpereannor@gmail.com>
2020-12-15 02:22:02 +0000

    feature: create getBoards action

--- src/state/board/actionCreators.js
+++ src/state/board/actionCreators.js
@@ -12,11 +12,29 @@ export const createBoard = boardData => dispatch => {
         dispatch({ 
             type: types.CREATE_BOARD_SUCCESS, payload: res.data
         })
-        console.log('boardData', res.data);
     })
     .catch(error => { 
         dispatch({ 
             type: types.CREATE_BOARD_FAILURE, payload: error.message
         })
     })
+}
+
+
+export const getBoards = () => dispatch => { 
+    dispatch({ type: types.REQUEST_START });
+    axiosWithAuth()
+    .get(`${boardUrl}user_boards`)
+    .then(res => { 
+        dispatch({ 
+            type: types.DISPLAY_BOARDS_SUCCESS,
+            payload: res.data
+        })
+    })
+    .catch(error => { 
+        dispatch({ 
+            type: types.DISPLAY_BOARDS_FAILURE,
+            payload: error.message
+        });
+    });
 }
