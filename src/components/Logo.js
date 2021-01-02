8965c9b6acaf7d821b659ab9aa9aac05d0e67507
chudnofskyATD <mpereannor@gmail.com>
2021-01-02 14:12:02 +0000

    style: add official logo

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
-
+import scuola_logo2_main  from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2_main.svg';
 const Logo = (props) => {
   return (
-    <img
-      alt="Logo"
-      src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIyNCIgaGVpZ2h0PSIyNCIgdmlld0JveD0iMCAwIDI0IDI0Ij48cGF0aCBkPSJNMjAgMTIuODc1djUuMDY4YzAgMi43NTQtNS43ODkgNC4wNTctOSA0LjA1Ny0zLjA1MiAwLTktMS4zOTItOS00LjA1N3YtNi4yOTRsOSA0Ljg2MyA5LTMuNjM3em0tOC4wODMtMTAuODc1bC0xMi45MTcgNS43NSAxMiA2LjUgMTEtNC40MTd2Ny4xNjdoMnYtOC4yNWwtMTIuMDgzLTYuNzV6bTEzLjA4MyAyMGgtNGMuNTc4LTEgMS0yLjUgMS00aDJjMCAxLjUxNi4zOTEgMi44NTkgMSA0eiIvPjwvc3ZnPg=="
-      {...props}
-    />
+      <img
+        style={{
+            "height":"50px", 
+    }}
+        src={ scuola_logo2_main }
+      >
+</img>
+
   );
 };
 
