b71e1533069c66635d666bac9707f7517ee8d506
chudnofskyATD <mpereannor@gmail.com>
2021-01-06 01:33:15 +0000

    chore: reconfigure main logo import

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import scuola_logo2_main  from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2_main.svg';
+// import scuola_logo2_main  from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2_main.svg';
+import scuola_logo2_main from '../assets/images/logo/scuola_logo2_main'
 const Logo = (props) => {
   return (
       <img
