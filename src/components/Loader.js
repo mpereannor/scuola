b0898a9987a83d935bcd701c317d314cce3d38a5
chudnofskyATD <mpereannor@gmail.com>
2021-01-06 01:43:00 +0000

    chore: reconfigure Loader imports

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import  "./Loader.css";
-import scuola_logo2a from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2a.svg';
-import scuola_logo2b from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2b.svg';
+// import scuola_logo2a from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2a.svg';
+// import scuola_logo2b from 'C:/Users/loki/Desktop/projects/scuola/src/assets/images/logo/scuola_logo2b.svg';
+import scuola_logo2a from '../assets/images/logo/scuola_logo2a.svg'
+import scuola_logo2b from '../assets/images/logo/scuola_logo2b.svg'
+
 
 export const Transition = () => { 
     return ( 
